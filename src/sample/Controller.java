4f30a0c1b5e0543226ec834a75e339160392a628
Nikos Katsilidis <katsilidisn@gmail.com>
2018-12-12 16:21:36 +0200

    button listeners

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+
+    public void selectBtn1(ActionEvent actionEvent) {
+        System.out.println("hello");
+    }
+
+
+    public void selectBtn2(ActionEvent actionEvent) {
+        System.out.println("hello");
+    }
+
+    public void selectBtn3(ActionEvent actionEvent) {
+        System.out.println("hello");
+    }
+
+    public void selectBtn4(ActionEvent actionEvent) {
+        System.out.println("hello");
+    }
+
+    public void selectBtn5(ActionEvent actionEvent) {
+        System.out.println("hello");
+    }
 }
