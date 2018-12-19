917037dd15fecb4825238c0352bb685822b2e9bf
Nikos Katsilidis <katsilidisn@gmail.com>
2018-12-19 17:18:10 +0200

    few changes

--- src/sample/DhlBapt.java
+++ src/sample/DhlBapt.java
@@ -1,4 +1,53 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.embed.swing.JFXPanel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DhlBapt {
+    public JFXButton menu_btn;
+
+    public void main_menu(ActionEvent actionEvent) {
+        Stage primaryStage = (Stage) menu_btn.getScene().getWindow();
+
+        primaryStage.close();
+
+        Stage stage = new Stage();
+
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+
+            AnchorPane pane = loader.load();
+
+
+            Scene scene = new Scene(pane);
+
+
+
+
+
+            scene.getStylesheets().addAll(Main.class.getResource("style.css").toExternalForm());
+
+            stage.setResizable(false);
+            stage.setTitle("Αρχική");
+            stage.setScene(scene);
+            stage.show();
+
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
