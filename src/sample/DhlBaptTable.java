b05bd3d6a0a717b02fd4983ef1cac361218d3bfb
Nikos Katsilidis <katsilidisn@gmail.com>
2018-12-21 19:45:53 +0200

    change names

--- src/sample/DhlBaptTable.java
+++ src/sample/DhlBaptTable.java
@@ -1,5 +1,13 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -7,6 +15,9 @@ import java.sql.SQLException;
 import java.util.prefs.Preferences;
 
 public class DhlBaptTable {
+
+    public JFXButton create_new_btn;
+    public JFXButton menu_btn;
 
     /*******************************
     //Σύνδεση με Βάση
@@ -26,4 +37,82 @@ public class DhlBaptTable {
     }
 
 
+    public void create_new(ActionEvent actionEvent) {
+
+        Stage primaryStage = (Stage) create_new_btn.getScene().getWindow();
+
+        primaryStage.close();
+
+        Stage stage = new Stage();
+
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("dhlBapt.fxml"));
+
+            AnchorPane pane = loader.load();
+
+
+            Scene scene = new Scene(pane);
+
+
+
+
+
+            scene.getStylesheets().addAll(Main.class.getResource("style.css").toExternalForm());
+
+            stage.setResizable(false);
+            stage.setTitle("Δήλωση Βαπτίσεως");
+            stage.setScene(scene);
+            stage.show();
+
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void main_menu(ActionEvent actionEvent) {
+
+        Stage primaryStage = (Stage) menu_btn.getScene().getWindow();
+
+        primaryStage.close();
+
+        Stage stage = new Stage();
+
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+
+            AnchorPane pane = loader.load();
+
+
+            Scene scene = new Scene(pane);
+
+
+
+
+
+            scene.getStylesheets().addAll(Main.class.getResource("style.css").toExternalForm());
+
+            stage.setResizable(false);
+            stage.setTitle("Αρχική");
+            stage.setScene(scene);
+            stage.show();
+
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
