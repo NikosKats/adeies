c64141f21f5535cc55a4a21886a91afc4518fdf7
Nikos Katsilidis <katsilidisn@gmail.com>
2018-12-21 18:21:32 +0200

    dhlwsh gamou finished

--- src/sample/DhlGamou.java
+++ src/sample/DhlGamou.java
@@ -1,11 +1,8 @@
 package sample;
 
 import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -169,10 +166,10 @@ public class DhlGamou {
         String sql = "INSERT INTO dataGamou(Field1,Field2,Field3,Field4,Field5,Field6,Field7,Field8,Field9,Field10," +
                 "Field11,Field12,Field13,Field14,Field15,Field16,Field17,Field18,Field19,Field20," +
                 "Field21,Field22,Field23,Field24,Field25,Field26,Field27,Field28,Field29,Field30," +
-                "Field31,Field32,Field33,Field34,Field35,Field36,Field37,Field38,Field39,Field40,)" +
+                "Field31,Field32,Field33,Field34,Field35,Field36,Field37,Field38,Field39,Field40," +
                 "Field41,Field42,Field43,Field44,Field45,Field46,Field47,Field48,Field49,Field50," +
                 "Field51,Field52,Field53,Field54,Field55,Field56,Field57,Field58,Field59,Field60," +
-                "Field61,Field62,Field63,Field64,Field65,Field66,Field67,Field68,Field69,Field70,)" +
+                "Field61,Field62,Field63,Field64,Field65,Field66,Field67,Field68,Field69,Field70," +
                 "Field71,Field72,Field73,Field74,Field75,Field76,Field77,Field78,Field79,Field80," +
                 "Field81,Field82,Field83,Field84,Field85,Field86,Field87,Field88," +
                 "CheckBox1,CheckBox2,CheckBox3,CheckBox4)" +
@@ -182,15 +179,15 @@ public class DhlGamou {
 
         try (Connection conn = this.connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, Field1);
-            pstmt.setString(2, Field2);
-            pstmt.setString(3, Field3);
-            pstmt.setString(4, Field4);
-            pstmt.setString(5, Field5);
-            pstmt.setString(6, Field6);
-            pstmt.setString(7, Field7);
-            pstmt.setString(8, Field8);
-            pstmt.setString(9, Field9);
+            pstmt.setString(1,  Field1);
+            pstmt.setString(2,  Field2);
+            pstmt.setString(3,  Field3);
+            pstmt.setString(4,  Field4);
+            pstmt.setString(5,  Field5);
+            pstmt.setString(6,  Field6);
+            pstmt.setString(7,  Field7);
+            pstmt.setString(8,  Field8);
+            pstmt.setString(9,  Field9);
             pstmt.setString(10, Field10);
             pstmt.setString(11, Field11);
             pstmt.setString(12, Field12);
