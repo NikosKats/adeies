88da177c7c019bbf69f127c0a07c2be939d10cf1
Nikos Katsilidis <katsilidisn@gmail.com>
2018-12-21 15:17:24 +0200

    dhlwsh gamou edits

--- src/sample/DhlGamou.java
+++ src/sample/DhlGamou.java
@@ -1,6 +1,7 @@
 package sample;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,6 +11,72 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class DhlGamou {
+
+
+    public JFXTextField field1;
+    public JFXTextField field2;
+    public JFXTextField field3;
+    public JFXTextField field4;
+    public JFXTextField field5;
+    public JFXTextField field6;
+    public JFXTextField field7;
+    public JFXTextField field8;
+    public JFXTextField field9;
+    public JFXTextField field10;
+    public JFXTextField field11;
+    public JFXTextField field12;
+    public JFXTextField field13;
+    public JFXTextField field14;
+    public JFXTextField field15;
+    public JFXTextField field16;
+    public JFXTextField field17;
+    public JFXTextField field18;
+    public JFXTextField field19;
+    public JFXTextField field20;
+    public JFXTextField field21;
+    public JFXTextField field22;
+    public JFXTextField field23;
+    public JFXTextField field24;
+    public JFXTextField field25;
+    public JFXTextField field26;
+    public JFXTextField field27;
+    public JFXTextField field28;
+    public JFXTextField field29;
+    public JFXTextField field30;
+    public JFXTextField field31;
+    public JFXTextField field32;
+    public JFXTextField field33;
+    public JFXTextField field34;
+    public JFXTextField field35;
+    public JFXTextField field36;
+    public JFXTextField field37;
+    public JFXTextField field38;
+    public JFXTextField field39;
+    public JFXTextField field40;
+    public JFXTextField field41;
+    public JFXTextField field42;
+    public JFXTextField field43;
+    public JFXTextField field44;
+    public JFXTextField field45;
+    public JFXTextField field46;
+    public JFXTextField field47;
+    public JFXTextField field48;
+    public JFXTextField field49;
+    public JFXTextField field50;
+    public JFXTextField field51;
+    public JFXTextField field52;
+    public JFXTextField field53;
+    public JFXTextField field54;
+    public JFXTextField field55;
+    public JFXTextField field56;
+    public JFXTextField field57;
+    public JFXTextField field58;
+    public JFXTextField field59;
+    public JFXTextField field60;
+    public JFXTextField field61;
+    public JFXTextField field62;
+
+
     public JFXButton menu_btn;
     public JFXButton view_btn;
 
@@ -89,4 +156,11 @@ public class DhlGamou {
             e.printStackTrace();
         }
     }
+
+
+
+
+
+
+
 }
