eeffe2bb6bce5632da5c7a94061058d345d4fc85
Nikos Katsilidis <katsilidisn@gmail.com>
2018-12-21 22:57:23 +0200

    databaptisis table select

--- src/sample/DhlBaptTable.java
+++ src/sample/DhlBaptTable.java
@@ -3,18 +3,23 @@ package sample;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
 import java.util.prefs.Preferences;
 
-public class DhlBaptTable {
+public class DhlBaptTable  {
+
+    int id;
+    String Field1, Field2, Field3, Field4, Field5, Field6, Field7, Field8, Field9, Field10, Field11, Field12, Field13, Field14, Field15, Field16, Field17, Field18, Field19, Field20, Field21, Field22, Field23, Field24, Field25, Field26, Field27, Field28, Field29, Field30, Field31;
+
 
     public JFXButton create_new_btn;
     public JFXButton menu_btn;
@@ -22,6 +27,7 @@ public class DhlBaptTable {
     public JFXButton edit_btn;
     public JFXButton delete_btn;
     public JFXButton refresh_btn;
+    public TableView tableView;
 
     /*******************************
     //Σύνδεση με Βάση
@@ -38,6 +44,61 @@ public class DhlBaptTable {
             System.out.println(e.getMessage());
         }
         return conn;
+    }
+
+    /*******************************
+    //Επιλογή από τη Βάση
+    /*******************************/
+
+    public void selectAll(){
+        String sql = "SELECT * FROM dataBaptisis";
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+
+                id = rs.getInt("id");
+                Field1 = rs.getString("Field1");
+                Field2 = rs.getString("Field2");
+                Field3 = rs.getString("Field3");
+                Field4 = rs.getString("Field4");
+                Field5 = rs.getString("Field5");
+                Field6 = rs.getString("Field6");
+                Field7 = rs.getString("Field7");
+                Field8 = rs.getString("Field8");
+                Field9 = rs.getString("Field9");
+                Field10 = rs.getString("Field10");
+                Field11 = rs.getString("Field11");
+                Field12 = rs.getString("Field12");
+                Field13 = rs.getString("Field13");
+                Field14 = rs.getString("Field14");
+                Field15 = rs.getString("Field15");
+                Field16 = rs.getString("Field16");
+                Field17 = rs.getString("Field17");
+                Field18 = rs.getString("Field18");
+                Field19 = rs.getString("Field19");
+                Field20 = rs.getString("Field20");
+                Field21 = rs.getString("Field21");
+                Field22 = rs.getString("Field22");
+                Field23 = rs.getString("Field23");
+                Field24 = rs.getString("Field24");
+                Field25 = rs.getString("Field25");
+                Field26 = rs.getString("Field26");
+                Field27 = rs.getString("Field27");
+                Field28 = rs.getString("Field28");
+                Field29 = rs.getString("Field29");
+                Field30 = rs.getString("Field30");
+                Field3 = rs.getString("Field31");
+
+
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 
@@ -131,4 +192,7 @@ public class DhlBaptTable {
 
     public void refresh(ActionEvent actionEvent) {
     }
+
+
+
 }
