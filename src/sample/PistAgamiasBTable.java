408cdd2d722bb1786dbd0c81cf7350acfbccdbad
Nikos Katsilidis <katsilidisn@gmail.com>
2018-12-21 19:49:09 +0200

    change names

--- src/sample/PistAgamiasBTable.java
+++ src/sample/PistAgamiasBTable.java
@@ -1,4 +1,91 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class PistAgamiasBTable {
+    public JFXButton create_new_btn;
+    public JFXButton menu_btn;
+
+
+    public void create_new(ActionEvent actionEvent) {
+
+        Stage primaryStage = (Stage) create_new_btn.getScene().getWindow();
+
+        primaryStage.close();
+
+        Stage stage = new Stage();
+
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("pistAgamiasB.fxml"));
+
+            AnchorPane pane = loader.load();
+
+
+            Scene scene = new Scene(pane);
+
+
+
+            scene.getStylesheets().addAll(Main.class.getResource("style.css").toExternalForm());
+
+            stage.setResizable(false);
+            stage.setTitle("Πιστοποιητικό Αγαμίας Β");
+            stage.setScene(scene);
+            stage.show();
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void main_menu(ActionEvent actionEvent) {
+
+        Stage primaryStage = (Stage) menu_btn.getScene().getWindow();
+
+        primaryStage.close();
+
+        Stage stage = new Stage();
+
+
+
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+
+            AnchorPane pane = loader.load();
+
+
+            Scene scene = new Scene(pane);
+
+
+
+
+
+            scene.getStylesheets().addAll(Main.class.getResource("style.css").toExternalForm());
+
+            stage.setResizable(false);
+            stage.setTitle("Αρχική");
+            stage.setScene(scene);
+            stage.show();
+
+
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
