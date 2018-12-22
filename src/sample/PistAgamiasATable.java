6071182231711d0825914842b8fe4d3eae3d5033
Nikos Katsilidis <katsilidisn@gmail.com>
2018-12-22 13:02:28 +0200

    pistagamiasA table populate

--- src/sample/PistAgamiasATable.java
+++ src/sample/PistAgamiasATable.java
@@ -1,18 +1,104 @@
 package sample;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
 
-public class PistAgamiasATable {
+public class PistAgamiasATable implements Initializable {
 
     public JFXButton create_new_btn;
     public JFXButton menu_btn;
+    public TableView tableView;
+    public TableColumn col1,col2,col3,col4,col5,col6,col7,col8,col9;
+
+    int id;
+    String Field2, Field3, Field4, Field6, Field9, Field10, Field11, Field12;
+
+
+    ObservableList<PistAgamiasATableData> obList = FXCollections.observableArrayList();
+
+    /*******************************
+     //Σύνδεση με Βάση
+     ******************************/
+
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:adeies.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    /*******************************
+     //Επιλογή από τη Βάση
+     /*******************************/
+
+    public void selectAll(){
+        String sql = "SELECT * FROM dataAgamiasA";
+
+
+
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+
+                id = rs.getInt("id");
+                String s_id = Integer.toString(id);
+
+                Field2 = rs.getString("Field2");
+                Field3 = rs.getString("Field3");
+                Field4 = rs.getString("Field4");
+                Field6 = rs.getString("Field6");
+                Field9 = rs.getString("Field9");
+                Field10 = rs.getString("Field10");
+                Field11 = rs.getString("Field11");
+                Field12 = rs.getString("Field12");
+
+
+                obList.add(new PistAgamiasATableData(s_id,Field2,Field3,Field4,Field6,Field9,Field10,Field11,Field12));
+
+
+                col1.setCellValueFactory(new PropertyValueFactory<>("id"));
+                col2.setCellValueFactory(new PropertyValueFactory<>("Field2"));
+                col3.setCellValueFactory(new PropertyValueFactory<>("Field3"));
+                col4.setCellValueFactory(new PropertyValueFactory<>("Field4"));
+                col5.setCellValueFactory(new PropertyValueFactory<>("Field6"));
+                col6.setCellValueFactory(new PropertyValueFactory<>("Field9"));
+                col7.setCellValueFactory(new PropertyValueFactory<>("Field10"));
+                col8.setCellValueFactory(new PropertyValueFactory<>("Field11"));
+                col9.setCellValueFactory(new PropertyValueFactory<>("Field12"));
+
+                tableView.setItems(obList);
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 
     public void create_new(ActionEvent actionEvent) {
 
@@ -91,5 +177,10 @@ public class PistAgamiasATable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        selectAll();
     }
 }
--- src/sample/PistAgamiasATableData.java
+++ src/sample/PistAgamiasATableData.java
@@ -1,4 +1,131 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class PistAgamiasATableData {
+
+    SimpleStringProperty id;
+    private final SimpleStringProperty field2,field3,field4,field6,field9,field10,field11,field12;
+
+    public PistAgamiasATableData(String id, String field2, String field3, String field4, String field6, String field9, String field10, String field11, String field12) {
+        this.id = new SimpleStringProperty(id);
+        this.field2 = new SimpleStringProperty(field2);
+        this.field3 = new SimpleStringProperty(field3);
+        this.field4 = new SimpleStringProperty(field4);
+        this.field6 = new SimpleStringProperty(field6);
+        this.field9 = new SimpleStringProperty(field9);
+        this.field10 = new SimpleStringProperty(field10);
+        this.field11 = new SimpleStringProperty(field11);
+        this.field12 = new SimpleStringProperty(field12);
+
+    }
+
+    public String getId() {
+        return id.get();
+    }
+
+    public SimpleStringProperty idProperty() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id.set(id);
+    }
+
+    public String getField2() {
+        return field2.get();
+    }
+
+    public SimpleStringProperty field2Property() {
+        return field2;
+    }
+
+    public void setField2(String field2) {
+        this.field2.set(field2);
+    }
+
+    public String getField3() {
+        return field3.get();
+    }
+
+    public SimpleStringProperty field3Property() {
+        return field3;
+    }
+
+    public void setField3(String field3) {
+        this.field3.set(field3);
+    }
+
+    public String getField4() {
+        return field4.get();
+    }
+
+    public SimpleStringProperty field4Property() {
+        return field4;
+    }
+
+    public void setField4(String field4) {
+        this.field4.set(field4);
+    }
+
+    public String getField6() {
+        return field6.get();
+    }
+
+    public SimpleStringProperty field6Property() {
+        return field6;
+    }
+
+    public void setField6(String field6) {
+        this.field6.set(field6);
+    }
+
+    public String getField9() {
+        return field9.get();
+    }
+
+    public SimpleStringProperty field9Property() {
+        return field9;
+    }
+
+    public void setField9(String field9) {
+        this.field9.set(field9);
+    }
+
+    public String getField10() {
+        return field10.get();
+    }
+
+    public SimpleStringProperty field10Property() {
+        return field10;
+    }
+
+    public void setField10(String field10) {
+        this.field10.set(field10);
+    }
+
+    public String getField11() {
+        return field11.get();
+    }
+
+    public SimpleStringProperty field11Property() {
+        return field11;
+    }
+
+    public void setField11(String field11) {
+        this.field11.set(field11);
+    }
+
+    public String getField12() {
+        return field12.get();
+    }
+
+    public SimpleStringProperty field12Property() {
+        return field12;
+    }
+
+    public void setField12(String field12) {
+        this.field12.set(field12);
+    }
 }
+
